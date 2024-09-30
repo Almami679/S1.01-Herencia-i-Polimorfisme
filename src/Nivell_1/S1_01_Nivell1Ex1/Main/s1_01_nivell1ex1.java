d164713ca3f56a1a90d6764e62f84c396564dfe6
Almami989 <almami989@gmail.com>
2024-09-30 12:05:37 +0200

    Desarrollo del primer ejercicio y estructuracion del proyecto

--- src/Nivell_1/S1_01_Nivell1Ex1/Main/s1_01_nivell1ex1.java
+++ src/Nivell_1/S1_01_Nivell1Ex1/Main/s1_01_nivell1ex1.java
@@ -1,10 +1,16 @@
-package S1_01_Nivell1Ex1;
+package Nivell_1.S1_01_Nivell1Ex1.Main;
+
+import Nivell_1.S1_01_Nivell1Ex1.Modules.Corda;
+import Nivell_1.S1_01_Nivell1Ex1.Modules.Instrument;
+import Nivell_1.S1_01_Nivell1Ex1.Modules.Percussio;
+import Nivell_1.S1_01_Nivell1Ex1.Modules.Vent;
 
 import java.util.ArrayList;
 
+
 public class s1_01_nivell1ex1 {
 
-    static ArrayList<Instrument> instruments = new ArrayList<Instrument>();
+    static ArrayList<Instrument> instruments = new ArrayList<>();
 
     public static void main(String[] args) {
         instruments.add(new Vent("Flauta travessera", 280));
--- src/Nivell_1/S1_01_Nivell1Ex1/Modules/Vent.java
+++ src/Nivell_1/S1_01_Nivell1Ex1/Modules/Vent.java
@@ -1,4 +1,4 @@
-package S1_01_Nivell1Ex1;
+package Nivell_1.S1_01_Nivell1Ex1.Modules;
 
 public class Vent extends Instrument {
 
